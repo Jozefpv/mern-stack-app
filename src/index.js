973584b3d3190d3b6128370bafe4bde28f29287c
Jozefpv <jozefpv99@gmail.com>
2022-10-23 20:42:44 +0100

    arreglo servidor

--- src/index.js
+++ src/index.js
@@ -22,4 +22,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 //inicio del servidor
 app.listen(app.get('port'), () => { 
     console.log(`Server on port ${app.get('port')}`)
+    
 })
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -4,9 +4,10 @@ const router = express.Router()
 
 const Task = require('../models/task')
 router.get('/', async (req, res) => {
-    const task = await Task.find()
-    console.log(task)
-    res.json(task)
+    //const task = await Task.find()
+    //console.log(task)
+    //res.json(task)
+    console.log("hola")
 })
 
 router.post('/', async (req, res) => {
